6520084aad1d0d3ffcc28651c41b5a84f7ab2e8a
Malin <maa.lin@outlook.com>
2026-09-04 14:09:24 +0200

    Initialize productservice.

---
+++ Sweditech.Console.App/Products/Services/ProductService.cs
@@ -0,0 +1,19 @@
+﻿
+using Sweditech.Console.App.Products.Interfaces;
+using Sweditech.Console.App.Products.Models;
+
+namespace Sweditech.Console.App.Products.Services;
+
+internal class ProductService : IProductService
+{
+    private readonly List<Product> _products = [];
+    public void AddProduct(string name, decimal price)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IReadOnlyList<Product> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+}
