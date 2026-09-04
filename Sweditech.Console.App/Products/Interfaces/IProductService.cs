4068dfadec05bcb0edd66a13f1669125345a33b2
Malin <maa.lin@outlook.com>
2026-09-04 13:56:43 +0200

    Add a product interface.

---
+++ Sweditech.Console.App/Products/Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿
+using Sweditech.Console.App.Products.Models;
+
+namespace Sweditech.Console.App.Products.Interfaces
+{
+    public interface IProductService
+    {
+        void AddProduct(string name, decimal price);
+        IReadOnlyList<Product> GetAllProducts();
+    }
+}
