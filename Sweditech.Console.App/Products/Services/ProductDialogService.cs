6d6794366fdae6224d2b5e31e8b49860ef4f32fb
Malin <maa.lin@outlook.com>
2026-09-04 15:39:15 +0200

    Add product dialog for adding an showing products.

--- Sweditech.Console.App/Products/Services/ProductDialogService.cs
+++ Sweditech.Console.App/Products/Services/ProductDialogService.cs
@@ -1,17 +1,95 @@
 ﻿
 using Sweditech.ConsoleApp.Products.Interfaces;
+using Sweditech.ConsoleApp.Products.Models;
 
 namespace Sweditech.ConsoleApp.Products.Services;
 
-internal class ProductDialogService(IProductService productService) : IProductDialog
+public class ProductDialogService(IProductService productService) : IProductDialog
 {
+    public void MenuDialog()
+    {
+        string menuSelection;
+
+        do
+        {
+            List<string> menu = [
+                "1. Lägg till produkt",
+                "2. Visa alla produkter",
+                "0. Avsluta"
+            ];
+
+            menu.ForEach(menuItem => Console.WriteLine(menuItem));
+
+            Console.WriteLine();
+            menuSelection = Console.ReadLine() ?? "";
+
+            switch (menuSelection)
+            {
+                case "1":
+                    AddProductDialog();
+                    break;
+                case "2":
+                    ShowAllProductsDialog();
+                    break;
+                case "0":
+                    break;
+            } 
+        }
+        while(menuSelection == "1" || menuSelection == "2");
+    }
     public void AddProductDialog()
     {
-        throw new NotImplementedException();
+        string name;
+
+        do
+        {
+            Console.Clear();
+            Console.Write("Ange produktnamn: ");
+            name = Console.ReadLine() ?? "";
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("--- Du måste ange ett giligt namn ---");
+            }
+
+        }
+        while (string.IsNullOrWhiteSpace(name));
+
+        decimal price;
+        bool parseSuccess;
+
+        do
+        {
+            Console.Write("Ange pris: ");
+            parseSuccess = decimal.TryParse(Console.ReadLine(), out price);
+
+            if (!parseSuccess || price <= 0)
+            {
+                Console.WriteLine("--- Du måste ange ett nummer större än 0 ---");
+            }
+        }
+        while (!parseSuccess || price <= 0);
+
+        productService.AddProduct(name, price);
+
+        Console.Clear();
+        Console.WriteLine($"{name} har lagts till");
+        Console.WriteLine();
     }
 
     public void ShowAllProductsDialog()
     {
-        throw new NotImplementedException();
+        IReadOnlyList<Product> products = productService.GetAllProducts();
+
+        Console.WriteLine("=== Products ===");
+        foreach (var product in products)
+        {
+            Console.WriteLine($"Id: {product.Id}");
+            Console.WriteLine($"Produktnamn: {product.Name}");
+            Console.WriteLine($"Pris: {product.Price}kr");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
     }
 }
--- Sweditech.Console.App/Program.cs
+++ Sweditech.Console.App/Program.cs
@@ -1,1 +1,6 @@
 ﻿
+using Sweditech.ConsoleApp.Products.Services;
+
+var productService = new ProductService();
+var productDialogService = new ProductDialogService(productService);
+productDialogService.MenuDialog();
